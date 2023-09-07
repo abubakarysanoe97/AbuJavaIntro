f1f1026420b4a5ee9801c79bb0d14abd3678ed65
abubakarysanoe97 <abubakarysanoe97@gmail.com>
2023-09-07 13:11:56 -0400

    Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Abubakary";
+        String lastName = "Sanoe";
+        System.out.println("First Name: " + firstName + "\nLast Name: " + lastName);
+    }
+}
